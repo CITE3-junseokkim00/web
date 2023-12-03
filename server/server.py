22b0263dadb4f599e0bc11e7eb44df6696416c23
junseok Kim <junseokkim00@berkeley.edu>
2023-12-03 17:45:29 +0900

    change format

--- server/server.py
+++ server/server.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 
 import Generation
-import KeyphraseExtraction
+# import KeyphraseExtraction
 import Extract
 import Summarization
 # import Distractor
@@ -19,7 +19,7 @@ CORS(app)
 
 
 @app.route("/api/makeChunk/<text>", methods=["GET"])
-def makeChunk(text: str):
+def makeChunk(text):
     response = doc2Chunk(text)
     json_obj = {"response": response}
     return jsonify(json_obj)
@@ -31,14 +31,14 @@ def read_root():
 
 
 @app.route("/api/summarization/<text>", methods=["GET"])
-def summary(text: str):
+def summary(text):
     return Summarization.query({"inputs": text,
                                 "parameters": {"min_length": 200, "max_length": 256, "repetition_penalty": 2.0},
                                 'options': {"wait_for_model": True}})
 
 
 @app.route("/api/extract/<text>", methods=["GET"])
-def extract(text: str):
+def extract(text):
     # response = KeyphraseExtraction.keywordExtraction(text)
     response = Extract.extract(document=text, model_name="gpt-3.5-turbo-0613", n_words=10)
     json_obj = {"response": response}
@@ -46,19 +46,19 @@ def extract(text: str):
 
 
 @app.route("/api/generation/<text>/<answer>", methods=["GET"])
-def generate(text: str, answer: str):
+def generate(text, answer):
     return Generation.query({"inputs": text + "<unused0>" + answer,
                              'options': {"wait_for_model": True}})
 
 @app.route("/api/distractors/<text>/<keyword>", methods=["GET"])
-def distractor(text: str, keyword: str):
+def distractor(text, keyword):
     # response = Distractor.get_distractor(text=text, keyword=keyword)
     response = distract(question=text, answer=keyword, model_name="gpt-3.5-turbo-0613")
     json_obj = {"response": response}
     return jsonify(json_obj)
 
 @app.route("/api/makeQuiz/<text>", methods=["GET"])
-def makeQuiz(text: str):
+def makeQuiz(text):
     questions = []
     summarize_response = Summarization.query({"inputs": text,
                                 "parameters": {"min_length": 200, "max_length": 256, "repetition_penalty": 2.0},
